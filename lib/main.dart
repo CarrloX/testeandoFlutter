bcc25660a5b880b71983fcf6a8f32302967df4f8
aquilessaltoasucama <aquilessaltoasucama@gmail.com>
2024-07-09 15:43:09 +0000

    jelou

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,13 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('resultado',
+          style: TextStyle(fontSize: 50, color: Color.fromARGB(225, 255, 255, 255)),),
+          backgroundColor: Colors.blueGrey,
+          toolbarHeight: 200,
+        ),
         body: const Center(
           child: Text('Hola mundo!'),
         ),
@@ -21,13 +28,36 @@ class MainApp extends StatelessWidget {
               ElevatedButton(
                 onPressed: null,
                 style: ElevatedButton.styleFrom(
-                  disabledForegroundColor: Color.fromARGB(255, 71, 71, 71).withOpacity(0.99),
+                  disabledForegroundColor: const Color.fromARGB(255, 71, 71, 71).withOpacity(0.99),
                   disabledBackgroundColor: const Color.fromARGB(255, 58, 243, 33).withOpacity(0.30),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(30),
                   ),
+                  visualDensity: VisualDensity.adaptivePlatformDensity,
                 ),
                 child: const Text('Calculadora'),
+              ),
+                            ElevatedButton(
+                onPressed: null,
+                style: ElevatedButton.styleFrom(
+                  disabledForegroundColor: const Color.fromARGB(255, 71, 71, 71).withOpacity(0.99),
+                  disabledBackgroundColor: const Color.fromARGB(255, 33, 159, 243).withOpacity(0.30),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: const Text('IMC'),
+              ),
+                            ElevatedButton(
+                onPressed: null,
+                style: ElevatedButton.styleFrom(
+                  disabledForegroundColor: const Color.fromARGB(255, 71, 71, 71).withOpacity(0.99),
+                  disabledBackgroundColor: const Color.fromARGB(255, 243, 33, 33).withOpacity(0.30),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: const Text('AC'),
               ),
             ],
           ),
