65dfbccdc724ce7c360574e9ed5660635b9a7de2
aquilessaltoasucama <aquilessaltoasucama@gmail.com>
2024-07-09 14:29:17 +0000

    boton

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,28 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
+        body: const Center(
           child: Text('Hola mundo!'),
         ),
         bottomNavigationBar: BottomAppBar(
-          child: Text('Bottom bar'),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: null,
+                style: ElevatedButton.styleFrom(
+                  disabledForegroundColor: Color.fromARGB(255, 71, 71, 71).withOpacity(0.99),
+                  disabledBackgroundColor: const Color.fromARGB(255, 58, 243, 33).withOpacity(0.30),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: const Text('Calculadora'),
+              ),
+            ],
+          ),
         ),
       ),
     );
